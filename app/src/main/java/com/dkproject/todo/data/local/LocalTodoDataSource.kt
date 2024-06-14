7e67b89a2138f88e7b766fb610384800a172154e
kdkd4 <kdkd422@naver.com>
2024-06-14 21:39:42 +0900

    로컬 데이터 소스

---
+++ app/src/main/java/com/dkproject/todo/data/local/LocalTodoDataSource.kt
@@ -0,0 +1,32 @@
+package com.dkproject.todoapp.data.local
+
+import com.dkproject.todoapp.data.model.TodoEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+
+
+class LocalTodoDataSource @Inject constructor(
+    private val todoDao: TodoDao
+){
+    suspend fun insertTodo(todo: TodoEntity) {
+        todoDao.insert(todo)
+    }
+
+    suspend fun deleteTodo(todo: TodoEntity) {
+        todoDao.delete(todo)
+    }
+
+    suspend fun updateTodo(todo: TodoEntity){
+        todoDao.update(todo)
+    }
+
+    fun getTodoById(id: Int): Flow<TodoEntity?> {
+        return todoDao.getTodoById(id)
+    }
+
+
+    fun getTodoByDate(date:Long) : Flow<List<TodoEntity>> {
+        return todoDao.dateTodo(date)
+    }
+}
