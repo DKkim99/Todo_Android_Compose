025377919c67c55396555e9af7d47459178abe82
kdkd4 <kdkd422@naver.com>
2024-06-14 21:37:30 +0900

    Data Store - 카테고리

---
+++ app/src/main/java/com/dkproject/todo/data/local/datastore/CategoryDataStore.kt
@@ -0,0 +1,39 @@
+package com.dkproject.todoapp.data.local.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "category_data")
+class CategoryDataStore @Inject constructor(
+    private val context: Context,
+    private val gson: Gson
+) {
+    companion object{
+        private val CATEGORY_KEY = stringPreferencesKey("category_data")
+    }
+
+    suspend fun saveCategoryData(categoryData:List<String>){
+        val categoryDataString = gson.toJson(categoryData)
+        context.dataStore.edit {mutablePreferences->
+            mutablePreferences[CATEGORY_KEY] = categoryDataString
+        }
+    }
+
+    fun getCategoryData(): Flow<List<String>>{
+        return context.dataStore.data.map { preferences ->
+            gson.fromJson(preferences[CATEGORY_KEY], object:TypeToken<List<String>>(){}.type) ?: emptyList()
+        }
+    }
+
+
+}
