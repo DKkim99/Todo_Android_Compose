7a27aab647f031d011cc0240b4b29b1fa0624636
kdkd4 <kdkd422@naver.com>
2024-06-14 21:37:14 +0900

    앱 모듈

---
+++ app/src/main/java/com/dkproject/todo/data/DI/AppModule.kt
@@ -0,0 +1,76 @@
+package com.dkproject.todoapp.data.DI
+
+import android.content.Context
+import androidx.room.Room
+import com.dkproject.todoapp.data.local.LocalTodoDataSource
+import com.dkproject.todoapp.data.local.TodoDao
+import com.dkproject.todoapp.data.local.TodoDatabase
+import com.dkproject.todoapp.domain.repository.CategoryRepository
+import com.dkproject.todoapp.domain.repository.TodoRepository
+import com.dkproject.todoapp.domain.usecase.GetCategoryUseCase
+import com.dkproject.todoapp.domain.usecase.GetTodoByDateUseCase
+import com.dkproject.todoapp.domain.usecase.InsertTodoUseCase
+import com.dkproject.todoapp.domain.usecase.SetCategoryUseCase
+import com.dkproject.todoapp.domain.usecase.UpdateTodoUseCase
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideGson():Gson{
+        return Gson()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context:Context):TodoDatabase{
+        return Room.databaseBuilder(
+            context,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    fun provideTodoDao(db:TodoDatabase) :TodoDao = db.todoDao()
+
+
+    @Provides
+    fun provideLocalTodoDataSource(todoDao: TodoDao): LocalTodoDataSource {
+        return LocalTodoDataSource(todoDao)
+    }
+
+    @Provides
+    fun provideGetTodoByDate(todoRepository: TodoRepository):GetTodoByDateUseCase{
+        return GetTodoByDateUseCase(todoRepository)
+    }
+
+    @Provides
+    fun provideInsertTodoUseCase(todoRepository: TodoRepository):InsertTodoUseCase{
+        return InsertTodoUseCase(todoRepository)
+    }
+
+    @Provides
+    fun provideUpdateTodoUseCase(todoRepository: TodoRepository):UpdateTodoUseCase{
+        return UpdateTodoUseCase(todoRepository)
+    }
+
+    @Provides
+    fun provideGetCategoryUseCase(categoryRepository:CategoryRepository):GetCategoryUseCase{
+        return GetCategoryUseCase(categoryRepository = categoryRepository)
+    }
+
+    @Provides
+    fun provideSetCategoryUseCase(categoryRepository: CategoryRepository) : SetCategoryUseCase{
+        return SetCategoryUseCase(categoryRepository = categoryRepository)
+    }
+}
