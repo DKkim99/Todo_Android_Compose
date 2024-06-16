7bc9b05ebf84de028abf869d850ffff0e016c467
kdkd4 <kdkd422@naver.com>
2024-06-16 13:52:01 +0900

    카테고리 저장소  반환 타입 수정

--- app/src/main/java/com/dkproject/todo/domain/repository/CategoryRepository.kt
+++ app/src/main/java/com/dkproject/todo/domain/repository/CategoryRepository.kt
@@ -1,9 +1,10 @@
 package com.dkproject.todoapp.domain.repository
 
+import com.dkproject.todo.domain.model.CategoryInfo
 import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
-    suspend fun setCategory(categoryData:List<String>)
+    suspend fun setCategory(categoryData:List<CategoryInfo>)
 
-    fun getCategory():Flow<List<String>>
+    fun getCategory():Flow<List<CategoryInfo>>
 }
