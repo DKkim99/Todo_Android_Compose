823200c7ec297e15ba266cb3ca7bf8b511f8ceb0
kdkd4 <kdkd422@naver.com>
2024-06-16 23:30:57 +0900

    Todo 네비게이션

--- app/src/main/java/com/dkproject/todo/presentation/navigation/TodoNavigation.kt
+++ app/src/main/java/com/dkproject/todo/presentation/navigation/TodoNavigation.kt
@@ -2,6 +2,7 @@ package com.dkproject.todoapp.presentation.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.TaskAlt
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -14,18 +15,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.dkproject.todoapp.presentation.ui.screen.Home.HomeScreen
 
 
-enum class TodoNavigationGraph(val description:String,val icon:ImageVector) {
-    HOME(description = "Todo",Icons.Default.TaskAlt),
-    CALENDAR(description = "Calendar",Icons.Default.CalendarMonth)
+enum class TodoNavigationGraph(val description: String, val icon: ImageVector) {
+    HOME(description = "Todo", Icons.Default.TaskAlt),
+    CALENDAR(description = "Calendar", Icons.Default.CalendarMonth),
 }
-
 
 
 @Composable
@@ -33,7 +35,7 @@ fun TodoNavigation(
     navHostController: NavHostController = rememberNavController()
 ) {
     Scaffold(bottomBar = {
-       TodoNavigationBottomBar(navController = navHostController)
+        TodoNavigationBottomBar(navController = navHostController)
     }) { innerPadding ->
         NavHost(
             modifier = Modifier,
@@ -42,6 +44,7 @@ fun TodoNavigation(
             composable(route = TodoNavigationGraph.HOME.name) {
                 HomeScreen(innerPadding = innerPadding)
             }
+
             composable(route = TodoNavigationGraph.CALENDAR.name) {
 
             }
@@ -52,15 +55,15 @@ fun TodoNavigation(
 @Composable
 fun TodoNavigationBottomBar(
     navController: NavController
-){
+) {
     val navigationItem = listOf(TodoNavigationGraph.HOME, TodoNavigationGraph.CALENDAR)
     val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentItem : TodoNavigationGraph = TodoNavigationGraph.valueOf(
+    val currentItem: TodoNavigationGraph = TodoNavigationGraph.valueOf(
         backStackEntry?.destination?.route ?: TodoNavigationGraph.HOME.name
     )
-    NavigationBar(modifier=Modifier) {
-        navigationItem.forEach{item->
-            NavigationBarItem(selected = currentItem==item, onClick = { /*TODO*/ }, icon = {
+    NavigationBar(modifier = Modifier) {
+        navigationItem.forEach { item ->
+            NavigationBarItem(selected = currentItem == item, onClick = { /*TODO*/ }, icon = {
                 Icon(imageVector = item.icon, contentDescription = item.description)
             },
                 label = {
