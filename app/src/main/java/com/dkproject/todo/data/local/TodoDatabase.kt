95a7bdc6ccadc03167365186badd6a4505c5b86c
kdkd4 <kdkd422@naver.com>
2024-06-16 13:47:05 +0900

    Todo Database

--- app/src/main/java/com/dkproject/todo/data/local/TodoDatabase.kt
+++ app/src/main/java/com/dkproject/todo/data/local/TodoDatabase.kt
@@ -1,7 +1,11 @@
 package com.dkproject.todoapp.data.local
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import com.dkproject.todoapp.data.model.TodoEntity
 
 @Database(entities = [TodoEntity::class], version = 1, exportSchema = false)
@@ -9,3 +13,4 @@ abstract class TodoDatabase :RoomDatabase(){
 
     abstract fun todoDao():TodoDao
 }
+
