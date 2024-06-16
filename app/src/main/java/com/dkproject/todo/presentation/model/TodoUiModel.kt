b203db9d3b012056388eaa05664cf0fd343e2954
kdkd4 <kdkd422@naver.com>
2024-06-16 23:31:27 +0900

    TodoUiModel

---
+++ app/src/main/java/com/dkproject/todo/presentation/model/TodoUiModel.kt
@@ -0,0 +1,46 @@
+package com.dkproject.todo.presentation.model
+
+import android.os.Parcelable
+import com.dkproject.todoapp.domain.model.Todo
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class TodoUiModel(
+    val id:Int=0,
+    var category:String?=null,
+    var date:Long = 0L,
+    var title:String ="",
+    var hour: Int? = null,
+    var minute: Int? = null,
+    var completed:Boolean = false,
+    var categoryColor : Int = 0
+):Parcelable
+
+
+fun TodoUiModel.toDomainModel() : Todo{
+    return Todo(
+        id = id,
+        category = category,
+        date = date,
+        title = title,
+        hour = hour,
+        minute = minute,
+        completed = completed,
+        categoryColor = categoryColor
+    )
+}
+
+
+fun Todo.toUiModel() : TodoUiModel{
+    return TodoUiModel(
+        id = id,
+        category = category,
+        date = date,
+        title = title,
+        hour = hour,
+        minute = minute,
+        completed = completed,
+        categoryColor = categoryColor
+    )
+}
