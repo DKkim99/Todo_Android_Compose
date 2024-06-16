8f38c98fc3056f41d3faa5cfd00501c28b5c6a03
kdkd4 <kdkd422@naver.com>
2024-06-16 23:30:44 +0900

    Todo 데이터 모델

--- app/src/main/java/com/dkproject/todo/domain/model/Todo.kt
+++ app/src/main/java/com/dkproject/todo/domain/model/Todo.kt
@@ -5,8 +5,8 @@ import androidx.compose.ui.graphics.Color
 data class Todo(
     val id:Int=0,
     val category:String?=null,
-    val date:Long,
-    val title:String,
+    val date:Long = 0L,
+    val title:String ="",
     val hour: Int? = null,
     val minute: Int? = null,
     val completed:Boolean = false,
