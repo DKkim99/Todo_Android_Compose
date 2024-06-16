6e101a8e28167351a3d6086959296c57dd3ebf52
kdkd4 <kdkd422@naver.com>
2024-06-16 13:51:28 +0900

    카테고리 설정 UseCase

--- app/src/main/java/com/dkproject/todo/domain/usecase/SetCategoryUseCase.kt
+++ app/src/main/java/com/dkproject/todo/domain/usecase/SetCategoryUseCase.kt
@@ -1,9 +1,10 @@
 package com.dkproject.todoapp.domain.usecase
 
+import com.dkproject.todo.domain.model.CategoryInfo
 import com.dkproject.todoapp.domain.repository.CategoryRepository
 
 class SetCategoryUseCase (private val categoryRepository: CategoryRepository) {
-    suspend operator fun invoke(categoryData: List<String>) {
+    suspend operator fun invoke(categoryData: List<CategoryInfo>) {
         categoryRepository.setCategory(categoryData)
     }
 
