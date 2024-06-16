e9221506b96b32ba15eb9edb7cb07d4f4fa2a9b9
kdkd4 <kdkd422@naver.com>
2024-06-16 23:31:15 +0900

    Todo 저장소 - id로 가져오기 추가

--- app/src/main/java/com/dkproject/todo/domain/repository/TodoRepository.kt
+++ app/src/main/java/com/dkproject/todo/domain/repository/TodoRepository.kt
@@ -11,6 +11,8 @@ interface TodoRepository {
 
     suspend fun updateTodo(todo: Todo)
 
+    suspend fun updateCategoryAndColor(categoryName: String, newCategoryName:String, newColor: Int)
+
     fun getTodoById(id: Int): Flow<Todo?>
 
     fun getDateTodo(date:Long): Flow<List<Todo>>
