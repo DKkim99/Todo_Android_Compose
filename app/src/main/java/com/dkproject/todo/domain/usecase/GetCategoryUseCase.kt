fb95d9c932fe5bdb53b1f25cfe4c4190860e6766
kdkd4 <kdkd422@naver.com>
2024-06-16 13:51:21 +0900

    카테고리 가져오기 UseCase

--- app/src/main/java/com/dkproject/todo/domain/usecase/GetCategoryUseCase.kt
+++ app/src/main/java/com/dkproject/todo/domain/usecase/GetCategoryUseCase.kt
@@ -1,10 +1,11 @@
 package com.dkproject.todoapp.domain.usecase
 
+import com.dkproject.todo.domain.model.CategoryInfo
 import com.dkproject.todoapp.domain.repository.CategoryRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetCategoryUseCase (private val categoryRepository: CategoryRepository) {
-    operator fun invoke() : Flow<List<String>>{
+    operator fun invoke() : Flow<List<CategoryInfo>>{
         return categoryRepository.getCategory()
     }
 }
