268c89864e00e0814c52b37ad9050e5e673824d4
kdkd4 <kdkd422@naver.com>
2024-06-16 13:52:35 +0900

    Todo 바텀시트 - 카테고리 기능 추가

--- app/src/main/java/com/dkproject/todo/presentation/ui/components/AddTodoBottomSheet.kt
+++ app/src/main/java/com/dkproject/todo/presentation/ui/components/AddTodoBottomSheet.kt
@@ -3,12 +3,15 @@ package com.dkproject.todoapp.presentation.ui.components
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.captionBar
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
@@ -18,12 +21,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.waterfall
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Upload
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -31,11 +37,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -54,7 +62,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.dkproject.todo.R
+import com.dkproject.todo.presentation.ui.screen.home.CategoryViewModel
+import com.dkproject.todo.presentation.util.ColorData
 import com.dkproject.todoapp.domain.model.Todo
 import com.dkproject.todoapp.presentation.util.dateFormatter
 import com.dkproject.todoapp.presentation.util.getTodayDateWithoutTime
@@ -65,9 +76,12 @@ import kotlinx.coroutines.launch
 @Composable
 fun AddTodoBottomSheet(
     modifier: Modifier = Modifier,
+    viewModel: CategoryViewModel = hiltViewModel(),
     onDismiss: () -> Unit,
     uploadTodo: (Todo) -> Unit,
 ) {
+    val categories by viewModel.categoryList.collectAsState()
+    var expandedCategory by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val snackbarHostState = remember { SnackbarHostState() }
@@ -79,7 +93,7 @@ fun AddTodoBottomSheet(
     var todoDate by remember { mutableStateOf(getTodayDateWithoutTime()) }
     var hour by remember { mutableIntStateOf(0) }
     var minute by remember { mutableIntStateOf(0) }
-
+    var categoryColor by remember { mutableIntStateOf(ColorData.skyBlue) }
     var visibleDatePicker by remember { mutableStateOf(false) }
     if (visibleDatePicker) {
         DateDialog(
@@ -96,6 +110,7 @@ fun AddTodoBottomSheet(
     }
 
 
+
     ModalBottomSheet(
         modifier = Modifier,
         onDismissRequest = onDismiss,
@@ -104,77 +119,91 @@ fun AddTodoBottomSheet(
         Column(
             modifier = Modifier
         )
-            {
-                CustomTextField(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .focusRequester(focusRequester)
-                        .padding(horizontal = 18.dp),
-                    value = todoValue,
-                    isSingleLine = true,
-                    onValueChange = { todoValue = it },
-                    placeholder = { Text(text = stringResource(id = R.string.writetodo)) },
-                    colors = TextFieldDefaults.colors(
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    ),
-                    focusRequester = focusRequester
+        {
+            CustomTextField(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .padding(horizontal = 18.dp),
+                value = todoValue,
+                isSingleLine = true,
+                onValueChange = { todoValue = it },
+                placeholder = { Text(text = stringResource(id = R.string.writetodo)) },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                focusRequester = focusRequester
+            )
+
+            Row(
+                modifier = Modifier.padding(horizontal = 12.dp),// 키보드 및 내비게이션 바 패딩 처리,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                AssistChip(
+                    onClick = { expandedCategory = true },
+                    label = {
+                        Text(
+                            text = if (category.isEmpty()) stringResource(id = R.string.nocategory) else category,
+                        )
+                    },
+                    colors = AssistChipDefaults.assistChipColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                    border = AssistChipDefaults.assistChipBorder(enabled = false)
                 )
-
-                Row(
-                    modifier = Modifier.padding(horizontal = 12.dp).imePadding() ,// 키보드 및 내비게이션 바 패딩 처리,
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    AssistChip(
-                        onClick = { /*TODO*/ },
-                        label = {
-                            Text(
-                                text = if (category.isEmpty()) stringResource(id = R.string.nocategory) else category,
-                            )
-                        },
-                        colors = AssistChipDefaults.assistChipColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
-                        border = AssistChipDefaults.assistChipBorder(enabled = false)
-                    )
-                    AssistChip(
-                        onClick = {
-                            visibleDatePicker = true
-                        },
-                        label = {
-                            Text(text = dateFormatter(todoDate))
-                        },
-                        colors = AssistChipDefaults.assistChipColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
-                        border = AssistChipDefaults.assistChipBorder(enabled = false)
-                    )
-
-                    Spacer(modifier = Modifier.weight(1f))
-
-                    IconButton(
-                        onClick = {
-                            if (todoValue.isEmpty()) {
-                                scope.launch {
-                                    snackbarHostState.showSnackbar(message = context.getString(R.string.inputtitle))
-                                }
-                                return@IconButton
-                            }
-                            uploadTodo(
-                                Todo(
-                                    title = todoValue,
-                                    category = category,
-                                    date = todoDate,
-                                    hour = hour,
-                                    minute = minute
-                                )
-                            )
-
-                        }) {
-                        Icon(imageVector = Icons.Default.Upload, contentDescription = null)
+                DropdownMenu(
+                    expanded = expandedCategory,
+                    onDismissRequest = { expandedCategory = false }) {
+                    categories.forEach {
+                        DropdownMenuItem(
+                            text = { Text(text = it.category) },
+                            onClick = {
+                                category = it.category
+                                categoryColor = it.color
+                                expandedCategory = false
+                            })
                     }
                 }
-                AnimatedVisibility(visible = snackbarHostState.currentSnackbarData != null) {
-                    SnackbarHost(hostState = snackbarHostState)
+                AssistChip(
+                    onClick = {
+                        visibleDatePicker = true
+                    },
+                    label = {
+                        Text(text = dateFormatter(todoDate))
+                    },
+                    colors = AssistChipDefaults.assistChipColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                    border = AssistChipDefaults.assistChipBorder(enabled = false)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                IconButton(
+                    onClick = {
+                        if (todoValue.isEmpty()) {
+                            scope.launch {
+                                snackbarHostState.showSnackbar(message = context.getString(R.string.inputtitle))
+                            }
+                            return@IconButton
+                        }
+                        uploadTodo(
+                            Todo(
+                                title = todoValue,
+                                category = category,
+                                date = todoDate,
+                                hour = hour,
+                                minute = minute,
+                                categoryColor = categoryColor
+                            )
+                        )
+
+                    }) {
+                    Icon(imageVector = Icons.Default.Upload, contentDescription = null)
                 }
             }
+            AnimatedVisibility(visible = snackbarHostState.currentSnackbarData != null) {
+                SnackbarHost(hostState = snackbarHostState)
+            }
+        }
     }
 }
 
